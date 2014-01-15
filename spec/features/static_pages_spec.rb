040462225c8c5e3262f705281f771836dfbf79c7
Michael <michael@michael-VirtualBox.(none)>
2014-01-15 19:56:28 +0000

    Added new helper method **full_title**

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -11,9 +11,17 @@ describe "Static pages" do
     it "should have the title 'Home'" do
       visit 'static_pages/home'
       page.should have_selector('title', 
-        :text => "#{base_title} | Home", :visible => false)
+        :text => "#{base_title}", :visible => false)
+    end
+
+    it "should not have a custom page title" do
+      visit 'static_pages/home'
+      page.should_not have_selector('title', 
+        :text => "| Home", :visible => false)
     end
   end
+
+  
 
 
   describe "Help page" do
